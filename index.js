6068057623a0ef3ae130e95290053b3c6b3916f1
yiweihsu <yiweihsutw@gmail.com>
2019-05-09 23:54:10 +0200

    fix coding style

--- index.js
+++ index.js
@@ -1,100 +1,96 @@
-const _ = require('lodash');
-const axios = require('axios');
-const pinyin = require('pinyin');
+const _ = require('lodash')
+const axios = require('axios')
+const pinyin = require('pinyin')
 
-const cluster = require('./dict/cluster');
-const dictionary_url =
-	'https://raw.githubusercontent.com/yiweihsu/rhyme-machine/master/dict/phrase';
+const cluster = require('./dict/cluster')
+const dictionaryURL = 'https://raw.githubusercontent.com/yiweihsu/rhyme-machine/master/dict/phrase'
 
-function getPinyinWords(rhyme) {
-	let pinyinWord = pinyin(rhyme, {
-		style: pinyin.STYLE_NORMAL, // 設置拼音風格
-		heteronym: false, // 多音字
-	});
-	return pinyinWord;
+const getPinyinWords = (rhyme) => {
+  const pinyinWord = pinyin(rhyme, {
+    style: pinyin.STYLE_NORMAL,
+    heteronym: false
+  })
+  return pinyinWord
 }
 
-function getClusterGroups(word) {
-	let groupArr = [];
+const getClusterGroups = (word) => {
+  const groupArr = []
 
-	let last1Str = word[word.length - 1].join(); // 最後一個字
-	let last2Str = word[word.length - 2].join(); // 倒數第二個字
-	let last3Str; // 倒數第三個字
+  const last1Str = word[word.length - 1].join() // 最後一個字
+  const last2Str = word[word.length - 2].join() // 倒數第二個字
+  let last3Str // 倒數第三個字
 
-	if (word.length > 2) {
-		last3Str = word[word.length - 3].join();
-	}
+  if (word.length > 2) {
+    last3Str = word[word.length - 3].join()
+  }
 
-	// check if the word belong to the group
-	for (let group in cluster) {
-		if (cluster[group].includes(last1Str)) {
-			groupArr['0'] = group;
-		}
-		if (cluster[group].includes(last2Str)) {
-			groupArr['1'] = group;
-		}
-		if (word.length > 2) {
-			if (cluster[group].includes(last3Str)) {
-				groupArr['2'] = group;
-			}
-		}
-	}
-	return groupArr;
+  // check if the word belongs to the group
+  for (const group in cluster) {
+    if (cluster[group].includes(last1Str)) {
+      groupArr['0'] = group
+    }
+    if (cluster[group].includes(last2Str)) {
+      groupArr['1'] = group
+    }
+    if (word.length > 2) {
+      if (cluster[group].includes(last3Str)) {
+        groupArr['2'] = group
+      }
+    }
+  }
+  return groupArr
 }
 
-async function getDictDataByUrl(url) {
-	try {
-		let response = await axios.get(url);
-		let data = response.data;
-		let arr1 = data.split(' ')[0].split('\n');
-		let arr2 = data.split(' ')[1].split('\n');
-		let result = _.concat(arr1, arr2);
-		return result;
-	} catch (error) {
-		throw error;
-	}
+const getDictDataByUrl = async (url) => {
+  try {
+    const response = await axios.get(url)
+    const data = response.data
+    const arr1 = data.split(' ')[0].split('\n')
+    const arr2 = data.split(' ')[1].split('\n')
+    const result = _.concat(arr1, arr2)
+    return result
+  } catch (error) {
+    throw error
+  }
 }
 
-function getRhymes(data, groupArr) {
-	let result = [];
-	let len = groupArr.length;
+const getRhymes = (data, groupArr) => {
+  const result = []
+  const len = groupArr.length
 
-	for (let i in data) {
-		let temp = data[i];
-		let rh1 = getPinyinWords(temp[temp.length - 1]);
-		let rh2 = getPinyinWords(temp[temp.length - 2]);
-		let rh3 = getPinyinWords(temp[temp.length - 3]);
+  for (const i in data) {
+    const temp = data[i]
+    const rh1 = getPinyinWords(temp[temp.length - 1])
+    const rh2 = getPinyinWords(temp[temp.length - 2])
+    const rh3 = getPinyinWords(temp[temp.length - 3])
 
-		// 三韻
-		if (len === 3) {
-			if (
-				cluster[groupArr['0']].includes(rh1.join()) &&
-				cluster[groupArr['1']].includes(rh2.join()) &&
-				cluster[groupArr['2']].includes(rh3.join())
-			) {
-				result.push(temp);
-			}
-		}
+    // 三韻
+    if (len === 3) {
+      if (
+        cluster[groupArr['0']].includes(rh1.join()) && cluster[groupArr['1']].includes(rh2.join()) && cluster[groupArr['2']].includes(rh3.join())
+      ) {
+        result.push(temp)
+      }
+    }
 
-		// 雙韻
-		if (len === 2) {
-			if (
-				cluster[groupArr[0]].includes(rh1.join()) &&
-				cluster[groupArr[1]].includes(rh2.join())
-			) {
-				result.push(temp);
-			}
-		}
-	}
-	return result;
+    // 雙韻
+    if (len === 2) {
+      if (
+        cluster[groupArr[0]].includes(rh1.join()) && cluster[groupArr[1]].includes(rh2.join())
+      ) {
+        result.push(temp)
+      }
+    }
+  }
+  return result
 }
 
-exports.search = async function search(rhymeWords) {
-	let word = await getPinyinWords(rhymeWords);
-	let grouArr = await getClusterGroups(word);
-	let data = await getDictDataByUrl(dictionary_url);
-	let result = await getRhymes(data, grouArr);
-	return result;
-};
+exports.search = async function search (rhymeWords) {
+  const word = await getPinyinWords(rhymeWords)
+  const grouArr = await getClusterGroups(word)
+  const data = await getDictDataByUrl(dictionaryURL)
+  const result = await getRhymes(data, grouArr)
+  return result
+}
 
-// exports.search('暴力');
+// exports.search('暴力')
--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,36 @@
-const assert = require('assert');
+const assert = require('assert')
 
 // using rewire to get into private resources
-const rewire = require('rewire');
-const RMrewire = rewire('.././index.js');
-const getPinyinWords = RMrewire.__get__('getPinyinWords');
-const getClusterGroups = RMrewire.__get__('getClusterGroups');
-const getRhymes = RMrewire.__get__('getRhymes');
+const rewire = require('rewire')
+const RMrewire = rewire('.././index.js')
+const getPinyinWords = RMrewire.__get__('getPinyinWords')
+const getClusterGroups = RMrewire.__get__('getClusterGroups')
+const getRhymes = RMrewire.__get__('getRhymes')
 
-const rm = require('../index');
+const rm = require('../index')
 
-const rhymeWords = '測試';
+const rhymeWords = '測試'
 
-describe('getPinyinWords()', () => {
-	it('Returns correct pinyin', () => {
-		assert.equal('ce', getPinyinWords(rhymeWords)[0].toString());
-	});
-});
-
-describe('getClusterGroups()', () => {
-	it('returns an object as data', () => {
-		assert.equal('object', typeof getClusterGroups(getPinyinWords(rhymeWords)));
-	});
-});
-
-describe('getDictDataByUrl()', () => {
-	it('returns an object as data', () => {
-		assert.strictEqual('object', typeof getRhymes('url', 1));
-	});
-});
-
-describe('search()', () => {
-	it('returns rhymes', () => {
-		assert.equal('object', typeof rm.search(rhymeWords));
-		// TODO test if value is equal
-	});
-});
+describe('Rhyme machine testing', () => {
+  describe('getPinyinWords()', () => {
+    it('Returns correct pinyin', () => {
+      assert.strict.equal('ce', getPinyinWords(rhymeWords)[0].toString())
+    })
+  })
+  describe('getClusterGroups()', () => {
+    it('returns an object as data', () => {
+      assert.strict.equal('object', typeof getClusterGroups(getPinyinWords(rhymeWords)))
+    })
+  })
+  describe('getDictDataByUrl()', () => {
+    it('returns an object as data', () => {
+      assert.strict.equal('object', typeof getRhymes('url', 1))
+    })
+  })
+  describe('search()', () => {
+    it('returns rhymes', () => {
+      assert.strict.equal('object', typeof rm.search(rhymeWords))
+      // TODO test if value is equal
+    })
+  })
+})
