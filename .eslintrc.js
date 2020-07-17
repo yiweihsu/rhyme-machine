01c0c998ae833d829b769a697575eec94f7f727e
yiwei <yiweihsutw@gmail.com>
2020-07-17 21:20:54 +0200

    lint and prettify applied

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "google",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-        "semi": [2, "never"]
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: "google",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    semi: [2, "never"],
+  },
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,38 @@
-const assert = require('assert')
+const assert = require("assert");
 
 // using rewire to get into private resources
-const rewire = require('rewire')
-const RMrewire = rewire('.././index.js')
-const getPinyinWords = RMrewire.__get__('getPinyinWords')
-const getClusterGroups = RMrewire.__get__('getClusterGroups')
-const getRhymes = RMrewire.__get__('getRhymes')
+const rewire = require("rewire");
+const RMrewire = rewire(".././index.js");
+const getPinyinWords = RMrewire.__get__("getPinyinWords");
+const getClusterGroups = RMrewire.__get__("getClusterGroups");
+const getRhymes = RMrewire.__get__("getRhymes");
 
-const rm = require('../index')
+const rm = require("../index");
 
-const rhymeWords = '測試'
+const rhymeWords = "測試";
 
-describe('Rhyme machine testing', () => {
-  describe('getPinyinWords()', () => {
-    it('Returns correct pinyin', () => {
-      assert.strict.equal('ce', getPinyinWords(rhymeWords)[0].toString())
-    })
-  })
-  describe('getClusterGroups()', () => {
-    it('returns an object as data', () => {
-      assert.strict.equal('object', typeof getClusterGroups(getPinyinWords(rhymeWords)))
-    })
-  })
-  describe('getDictDataByUrl()', () => {
-    it('returns an object as data', () => {
-      assert.strict.equal('object', typeof getRhymes('url', 1))
-    })
-  })
-  describe('search()', () => {
-    it('returns rhymes', () => {
-      assert.strict.equal('object', typeof rm.search(rhymeWords))
-    })
-  })
-})
+describe("Rhyme machine testing", () => {
+  describe("getPinyinWords()", () => {
+    it("Returns correct pinyin", () => {
+      assert.strict.equal("ce", getPinyinWords(rhymeWords)[0].toString());
+    });
+  });
+  describe("getClusterGroups()", () => {
+    it("returns an object as data", () => {
+      assert.strict.equal(
+        "object",
+        typeof getClusterGroups(getPinyinWords(rhymeWords))
+      );
+    });
+  });
+  describe("getDictDataByUrl()", () => {
+    it("returns an object as data", () => {
+      assert.strict.equal("object", typeof getRhymes("url", 1));
+    });
+  });
+  describe("search()", () => {
+    it("returns rhymes", () => {
+      assert.strict.equal("object", typeof rm.search(rhymeWords));
+    });
+  });
+});
